656c628ff04a858e7ebbb776bfc3846ab2a3c3ad
Jani Järvinen <jani.jarvinen@intertechno.org>
2019-10-07 13:37:00 +0300

    Toteutettu yksinkertainen asiakaslistaus tietokannasta.

--- AspNetWebApplication/AspNetWebApplication/Controllers/CustomersController.cs
+++ AspNetWebApplication/AspNetWebApplication/Controllers/CustomersController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetWebApplication.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,5 +12,20 @@ namespace AspNetWebApplication.Controllers
     [ApiController]
     public class CustomersController : ControllerBase
     {
+        [HttpGet]
+        [Route("")]
+        public List<Customers> ListaaKaikki()
+        {
+            NorthwindContext context = new NorthwindContext();
+            List<Customers> kaikki = context.Customers.ToList();
+            return kaikki;
+        }
+
+        [HttpDelete]
+        [Route("")]
+        public bool PoistaAsiakkaat()
+        {
+            return false;
+        }
     }
 }
