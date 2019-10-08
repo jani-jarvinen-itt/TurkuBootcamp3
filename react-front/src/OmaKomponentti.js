9874db0d138a96811bdf540a6902461beadf5a02
Jani Järvinen <jani.jarvinen@intertechno.org>
2019-10-08 11:18:27 +0300

    Aloitettu lisäämään uutta React-komponenttia.

---
+++ react-front/src/OmaKomponentti.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class OmaKomponentti extends React.Component {
+    render() {
+        return <h1>Tämä on oma komponenttini!</h1>;
+    }
+}
+
+export default OmaKomponentti;
--- react-front/src/index.js
+++ react-front/src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import OmaKomponentti from './OmaKomponentti'
 
+// ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
