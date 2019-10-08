7f0c63f84e55df533d5d54b708c10f714ebddbfc
Jani Järvinen <jani.jarvinen@intertechno.org>
2019-10-08 14:12:14 +0300

    Lisätty CORS-politiikka backendiin.

--- AspNetWebApplication/AspNetWebApplication/Startup.cs
+++ AspNetWebApplication/AspNetWebApplication/Startup.cs
@@ -24,6 +24,13 @@ namespace AspNetWebApplication
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
@@ -38,6 +45,8 @@ namespace AspNetWebApplication
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors("MyPolicy");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
