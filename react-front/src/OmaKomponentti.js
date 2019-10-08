858d2c3dd5aa78b766c60ea528d14fe7d69922bf
Jani Järvinen <jani.jarvinen@intertechno.org>
2019-10-08 12:02:56 +0300

    Lisätty Bootstrap-kirjasto ja taulukon renderöinti.

--- react-front/src/OmaKomponentti.js
+++ react-front/src/OmaKomponentti.js
@@ -3,18 +3,28 @@ import React from 'react';
 class OmaKomponentti extends React.Component {
     render() {
 
-        var luvut = [];
+        var taulukko = [];
         for (let index = 1; index <= 10; index++) {
-            luvut.push(<p>{index}</p>);            
+            taulukko.push(<tr>
+                <td>{index}-1</td>
+                <td>{index}-2</td>
+                <td>{index}-3</td>
+            </tr>);            
         }
 
-        var lisäKoodia = <p>Tässä on tavallista tekstiä.</p>;
-
         return <div>
-            <h1>Tämä on oma komponenttini!</h1>
-            <p>Testi</p>
-            {lisäKoodia}
-            {luvut}
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Sarake 1</th>
+                        <th>Sarake 2</th>
+                        <th>Sarake 3</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {taulukko}
+                </tbody>
+            </table>
         </div>;
     }
 }
