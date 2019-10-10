844ae2eac620664661d31933799accabc06d3820
Jani Järvinen <jani.jarvinen@intertechno.org>
2019-10-10 11:35:13 +0300

    Lisätty MSTest-yksikkötestiprojekti ratkaisuun (solution).

---
+++ AspNetWebApplication/AspNetWebApplication.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AspNetWebApplication.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
