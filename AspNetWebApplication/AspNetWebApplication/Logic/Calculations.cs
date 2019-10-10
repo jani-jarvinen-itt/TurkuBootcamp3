481182d77d5a13d7a7db7a8e18fade4c58f5a9d9
Jani Järvinen <jani.jarvinen@intertechno.org>
2019-10-10 11:43:18 +0300

    Lisätty yksinkertainen Calculations-luokka.

---
+++ AspNetWebApplication/AspNetWebApplication/Logic/Calculations.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetWebApplication.Logic
+{
+    public class Calculations
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
