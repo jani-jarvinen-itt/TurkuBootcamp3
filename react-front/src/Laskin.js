63768139caa895050937670a221608afc282e99d
Jani Järvinen <jani.jarvinen@intertechno.org>
2019-10-14 15:05:16 +0300

    Lisätty oikeaoppiset Bind-lauseet React-komponentin tapahtumankäsittelijöihin.

--- react-front/src/Laskin.js
+++ react-front/src/Laskin.js
@@ -2,18 +2,33 @@ import React from 'react';
 
 class Laskin extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        console.log("Konstruktorissa...");
+
+        this.luku1Muuttui = this.luku1Muuttui.bind(this);
+        this.luku2Muuttui = this.luku2Muuttui.bind(this);
+        this.nappiaKlikattu = this.nappiaKlikattu.bind(this);
+    }
+
     nappiaKlikattu() {
         console.log("Laske-nappia klikattu!");
+        console.log("Tila nyt:");
+        console.log(this.state);
+
+        let summa = parseInt(this.state.luku1) + parseInt(this.state.luku2);
+        alert("Summa on: " + summa);
     }
 
     luku1Muuttui(event) {
         console.log("luku1Muuttui-metodissa");
-        this.setState({ luku1: event.target.value});
+        this.setState({ luku1: event.target.value });
     }
 
     luku2Muuttui(event) {
         console.log("luku2Muuttui-metodissa");
-        this.setState({ luku2: event.target.value});
+        this.setState({ luku2: event.target.value });
     }
 
     render() {
