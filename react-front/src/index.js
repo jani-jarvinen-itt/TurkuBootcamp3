56c46589c943437d14486dfba230899db0001e77
Jani Järvinen <jani.jarvinen@intertechno.org>
2019-10-14 11:16:32 +0300

    Korjattu React-varoitukset kommentoimalla import-lauseita.

--- react-front/src/index.js
+++ react-front/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import OmaKomponentti from './OmaKomponentti'
+// import OmaKomponentti from './OmaKomponentti'
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<App />, document.getElementById('root'));
