3f6226aab1ceb76b7ad8e577a38a9ca51674b601
Jani Järvinen <jani.jarvinen@intertechno.org>
2019-10-14 14:54:01 +0300

    Lisätty OnChange-tapahtumankäsittelijät (ensimmäinen versio).

--- react-front/src/Laskin.js
+++ react-front/src/Laskin.js
@@ -6,11 +6,21 @@ class Laskin extends React.Component {
         console.log("Laske-nappia klikattu!");
     }
 
+    luku1Muuttui(event) {
+        console.log("luku1Muuttui-metodissa");
+        this.setState({ luku1: event.target.value});
+    }
+
+    luku2Muuttui(event) {
+        console.log("luku2Muuttui-metodissa");
+        this.setState({ luku2: event.target.value});
+    }
+
     render() {
         return <div>
             <h1>Lomake-testi</h1>
-            <p> <input type="text" /> </p>
-            <p> <input type="text" /> </p>
+            <p> <input type="text" onChange={this.luku1Muuttui} /> </p>
+            <p> <input type="text" onChange={this.luku2Muuttui} /> </p>
             <p> <button onClick={this.nappiaKlikattu}>Laske</button> </p>
         </div>;
     }
